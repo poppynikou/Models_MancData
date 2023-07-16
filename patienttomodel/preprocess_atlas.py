f842519ef6bf3e5750d61a57811bb28cd67fbd28
poppynikou <poppyrebecca@hotmail.co.uk>
2023-07-16 15:23:39 +0100

    removing crop from alignment to atlas

--- patienttomodel/preprocess_atlas.py
+++ patienttomodel/preprocess_atlas.py
@@ -20,12 +20,5 @@ NewNiftiObj = nib.Nifti1Image(img_obj_copy, img_affine, img_header)
 new_pCT_hn_3_path = 'T:/Poppy/PatData/MASKED_average_pCT.nii.gz'
 nib.save(NewNiftiObj, new_pCT_hn_3_path)
 
-CT_path = new_pCT_hn_3_path
-Sform_matrix_path = 'T:/Poppy/PatData/Sform.txt'
-CT_CROPPED_path = 'T:/Poppy/PatData/cropped_MASKED_average_pCT.nii.gz'
-updated_img_path = 'T:/Poppy/PatData/cropped_MASKED_average_pCT.nii.gz'
-slice_cut = [75,101]
-crop_shift_CT(CT_path, Sform_matrix_path, CT_CROPPED_path, updated_img_path, slice_cut = slice_cut, Manc = False)
-os.remove(Sform_matrix_path)
 
 
--- patienttomodel/rigid.py
+++ patienttomodel/rigid.py
@@ -1,8 +1,13 @@
 # first align a patient rigidly 
 import numpy as np 
 import os
+'''
+I decided that you don't need to crop the images for the rigid and affine alignment
+You just need a good mask, to make sure that only the anatomy shown in the atlas is shown in the patient CT
+The initial alignment code just puts them into the same space 
+'''
 
-hn_atlas_path = 'T:/Poppy/PatData/cropped_MASKED_average_pCT.nii.gz'
+hn_atlas_path = 'T:/Poppy/PatData/MASKED_average_pCT.nii.gz'
 reg_aladin = 'T:/Poppy/niftireg_executables/reg_aladin.exe'
 reg_transform = 'T:/Poppy/niftireg_executables/reg_transform.exe'
 base_path = 'T:/Poppy/PatData/test/'
@@ -16,17 +21,18 @@ for patient in patients:
     pCT_hn_path = patienttomodel_path + 'InitAlignment_MASKED_rescaled_pCT.nii.gz'
 
     ref_img = hn_atlas_path
-    float_img =  patienttomodel_path + 'pCT_cropped.nii.gz'
+    #float_img =  patienttomodel_path + 'pCT_cropped.nii.gz'
+    float_img = pCT_hn_path
     transformation = patienttomodel_path + '/atlas_rigid.txt'
     resampled_img1 = patienttomodel_path + '/resampled_pCT_atlas_rigid.nii.gz'
     command = reg_aladin + ' -ref ' + ref_img + ' -flo ' + float_img + ' -aff ' + transformation + ' -res ' + resampled_img1 + ' -rigOnly -omp 12' 
     os.system(command)
 
     # delete resampled img, and update the Sform 
-    #os.remove(resampled_img)
+    os.remove(resampled_img1)
     
     # update the Sform of the non-cropped image 
-    img_to_be_updated = pCT_hn_path
+    img_to_be_updated = pCT_hn_path 
     affine = transformation
     updated_img = patienttomodel_path + '/RIGID_pCT.nii.gz'
     command = reg_transform + ' -ref ' + img_to_be_updated + ' -updSform ' + img_to_be_updated + ' ' + affine + ' ' + updated_img
@@ -34,14 +40,13 @@ for patient in patients:
 
 
     ref_img = hn_atlas_path
-    float_img = resampled_img1
+    float_img = updated_img
     transformation = patienttomodel_path + '/atlas_affine.txt'
     resampled_img2 = patienttomodel_path + '/resampled_pCT_atlas_affine.nii.gz'
     command = reg_aladin + ' -ref ' + ref_img + ' -flo ' + float_img + ' -aff ' + transformation + ' -res ' + resampled_img2 +  ' -omp 12' 
     os.system(command)
 
     # delete resampled img, and update the Sform 
-    os.remove(resampled_img1)
     os.remove(resampled_img2)
 
     # update the Sform of the non-cropped image 
@@ -51,5 +56,7 @@ for patient in patients:
     command = reg_transform + ' -ref ' + img_to_be_updated + ' -updSform ' + img_to_be_updated + ' ' + affine + ' ' + updated_img
     os.system(command)
     # first align a patient rigidly 
+
+
 
  
